fd671f7df30eb91658683f3fc4f57eedad0801f6
Kishan <yannainghtun112358@gmail.com>
2021-04-19 14:17:29 +0630

    hello.js

---
+++ hello.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const app = require("express")();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+    console.log("User connected");
+    socket.on("disconnect", () => {
+        console.log("Disconnected");
+    });
+    socket.on("chat", (msg) => {
+        console.log("Message " + msg);
+    });
+});
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/home.html");
+});
+server.listen(3000, "127.0.0.1", function() {
+    console.log("Listening..");
+});
