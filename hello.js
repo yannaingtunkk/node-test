2b579c04b9fd824d836533a907b60587b5e179d0
Kishan <yannainghtun112358@gmail.com>
2021-04-22 18:15:03 +0630

    origin

--- hello.js
+++ hello.js
@@ -2,19 +2,9 @@ const http = require("http");
 const app = require("express")();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-io.on("connection", (socket) => {
-    console.log("User connected");
-    socket.on("disconnect", () => {
-        console.log("Disconnected");
-    });
-    socket.on("chat", (msg) => {
-        console.log("Message " + msg);
-        io.emit("chat", msg);
-    });
-});
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/home.html");
 });
-server.listen(process.env.PORT, "0.0.0.0", function() {
+server.listen(process.env.PORT || 3000, "0.0.0.0", function() {
     console.log("Listening..");
 });
